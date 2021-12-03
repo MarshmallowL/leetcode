6de10251b761b0c65a849955e30e34fa0ef6e96c
liugs <2279629558@qq.com>
2021-12-03 22:39:13 +0800

    [414]第三大的数

---
+++ src/com/liugs/leetcode/editor/cn/ThirdMaximumNumber.java
@@ -0,0 +1,75 @@
+//给你一个非空数组，返回此数组中 第三大的数 。如果不存在，则返回数组中最大的数。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：[3, 2, 1]
+//输出：1
+//解释：第三大的数是 1 。 
+//
+// 示例 2： 
+//
+// 
+//输入：[1, 2]
+//输出：2
+//解释：第三大的数不存在, 所以返回最大的数 2 。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：[2, 2, 3, 1]
+//输出：1
+//解释：注意，要求返回第三大的数，是指在所有不同数字中排第三大的数。
+//此例中存在两个值为 2 的数，它们都排第二。在所有不同数字中排第三大的数为 1 。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -2³¹ <= nums[i] <= 2³¹ - 1 
+// 
+//
+// 
+//
+// 进阶：你能设计一个时间复杂度 O(n) 的解决方案吗？ 
+// Related Topics 数组 排序 👍 318 👎 0
+
+
+package com.liugs.leetcode.editor.cn;
+
+public class ThirdMaximumNumber {
+    public static void main(String[] args) {
+        Solution solution = new ThirdMaximumNumber().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int thirdMax(int[] nums) {
+            Integer a = null, b = null, c = null;
+            for (int num : nums) {
+                if (a == null || a < num) {
+                    c = b;
+                    b = a;
+                    a = num;
+                } else if (a > num && (b == null || b < num)) {
+                    c = b;
+                    b = num;
+                } else if (b != null && b > num && (c == null || c < num)) {
+                    c = num;
+                }
+            }
+            if (c == null) {
+                return a;
+            } else {
+                return c;
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
