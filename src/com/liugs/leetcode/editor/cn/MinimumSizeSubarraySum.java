2edfe00a3abc282f3304388c865c729e82936ce0
liugs <2279629558@qq.com>
2022-01-05 23:15:16 +0800

    ✔[209]长度最小的子数组 改进
    使用滑动窗口算法，减小遍历次数

--- src/com/liugs/leetcode/editor/cn/MinimumSizeSubarraySum.java
+++ src/com/liugs/leetcode/editor/cn/MinimumSizeSubarraySum.java
@@ -59,16 +59,18 @@ public class MinimumSizeSubarraySum {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int minSubArrayLen(int target, int[] nums) {
+            int left=0,right=0;
             int minLen = Integer.MAX_VALUE;
-            for (int i = 0, len = nums.length; i < len; i++) {
-                int count = 0;
-                for (int j = i; j < len; j++) {
-                    count += nums[j];
-                    if (count >= target) {
-                        minLen = Math.min(j-i+1, minLen);
-                        break;
-                    }
+            int len = nums.length;
+            int sum = 0;
+            while (right < len){
+                sum += nums[right];
+                while (sum >= target){
+                    minLen = Math.min(right-left+1,minLen);
+                    sum -= nums[left];
+                    left++;
                 }
+                right++;
             }
             return minLen == Integer.MAX_VALUE ? 0 : minLen;
         }
