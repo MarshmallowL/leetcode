6c0a1291b8c06876beca45298f212a6d0834b815
liugs <2279629558@qq.com>
2022-04-08 17:43:53 +0800

    ✔	[116]填充每个节点的下一个右侧节点指针
    1.如果是左子节点，则其下一个右侧节点指针应该指向其父节点的右子节点
    2.如果是右子节点，则其下一个右侧节点指针应该指向其父节点右侧节点指针指向节点的左子节点
    3.需要先计算当前节点的右侧节点

---
+++ src/com/liugs/leetcode/editor/cn/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,129 @@
+//给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+//
+// 
+//struct Node {
+//  int val;
+//  Node *left;
+//  Node *right;
+//  Node *next;
+//} 
+//
+// 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。 
+//
+// 初始状态下，所有 next 指针都被设置为 NULL。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [1,2,3,4,5,6,7]
+//输出：[1,#,2,3,#,4,5,6,7,#]
+//解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。序列化的输出按层序遍历排列，同一层节点由 
+//next 指针连接，'#' 标志着每一层的结束。
+// 
+//
+// 
+//
+// 示例 2: 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点的数量在 [0, 2¹² - 1] 范围内 
+// -1000 <= node.val <= 1000 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 你只能使用常量级额外空间。 
+// 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 链表 二叉树 👍 728 👎 0
+
+
+package com.liugs.leetcode.editor.cn;
+
+import com.liugs.leetcode.editor.cn.commonObj.Node;
+
+public class PopulatingNextRightPointersInEachNode {
+    public static void main(String[] args) {
+        Solution solution = new PopulatingNextRightPointersInEachNode().new Solution();
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
+        node1.left = node2;
+        node1.right = node3;
+        solution.connect(node1);
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+    class Solution {
+        public Node connect(Node root) {
+            if (root == null){
+                return null;
+            }
+            connected(root, null, true);
+            return root;
+        }
+
+        private void connected(Node root, Node parentNode, boolean isLeftNode) {
+            if (isLeftNode && parentNode != null){
+                root.next = parentNode.right;
+            }else if (!isLeftNode && parentNode != null){
+                root.next = parentNode.next != null ? parentNode.next.left : null;
+            }
+            if (root.left != null) {
+                connected(root.left, root, true);
+            }
+            if (root.right != null) {
+                connected(root.right, root, false);
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
