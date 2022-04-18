1ee31a93eb410cdac4ca9c5202418a09103e5419
liugs <2279629558@qq.com>
2022-04-18 22:48:52 +0800

    ✔	[141]环形链表
    快慢指针

---
+++ src/com/liugs/leetcode/editor/cn/commonObj/ListNode.java
@@ -0,0 +1,15 @@
+package com.liugs.leetcode.editor.cn.commonObj;
+
+/**
+ * @author liugs
+ * @createTime 2022/4/18 22:31
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
