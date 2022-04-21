abbee89049d8d539297c86771a243b09221addc0
liugs <2279629558@qq.com>
2022-04-21 22:33:09 +0800

    ✔	[19]删除链表的倒数第 N 个结点

---
+++ src/com/liugs/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,96 @@
+//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], n = 2
+//输出：[1,2,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1], n = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1,2], n = 1
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中结点的数目为 sz 
+// 1 <= sz <= 30 
+// 0 <= Node.val <= 100 
+// 1 <= n <= sz 
+// 
+//
+// 
+//
+// 进阶：你能尝试使用一趟扫描实现吗？ 
+// Related Topics 链表 双指针 👍 1981 👎 0
+
+
+package com.liugs.leetcode.editor.cn;
+
+import com.liugs.leetcode.editor.cn.commonObj.ListNode;
+
+public class RemoveNthNodeFromEndOfList {
+    public static void main(String[] args) {
+        Solution solution = new RemoveNthNodeFromEndOfList().new Solution();
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+//        node2.next = node3;
+//        node3.next = node4;
+//        node4.next = node5;
+        solution.removeNthFromEnd(node1, 1);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            if (head == null){
+                return null;
+            }
+            ListNode fast = head;
+            ListNode slow = head;
+            for (int i = 0; i < n; i++) {
+                fast = fast.next;
+            }
+            if (fast == null){
+                return head.next;
+            }
+            while (fast.next != null){
+                fast = fast.next;
+                slow = slow.next;
+            }
+            slow.next = slow.next.next;
+            return head;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
